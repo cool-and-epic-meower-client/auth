b98830dfa4bebff0fedf92af2749edd9ba040381
Finley <161162126+Finley224@users.noreply.github.com>
2024-09-04 09:28:10 -0400

    Get dbInfo from env
    Program now gets the db login info from the env, if it is missing, it exits with code 29.

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
 #!/usr/bin/node
 
-console.log("Hello World");
+// Get db info from env
+// Save the dbUrl if it has been given in env
+if (typeof process.env.dbUrl != "undefined") {
+    dbUrl = process.env.dbUrl;
+} else {
+    console.log("Error (29): dbUrl not provided");
+    process.exit(29);
+}
+
+// Save the dbUser if it has been given in env
+if (typeof process.env.dbUser != "undefined") {
+    dbUser = process.env.dbUser;
+} else {
+    console.log("Error (29): dbUser not provided");
+    process.exit(29);
+}
+
+// Save the dbPassword if it has been given in env
+if (typeof process.env.dbPassword != "undefined") {
+    dbPassword = process.env.dbPassword;
+} else {
+    console.log("Error (29): dbPassword not provided");
+    process.exit(29);
+}
+
+console.log("Test line");
